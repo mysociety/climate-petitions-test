c72e1d8d75fcca44b7fcd281fa142b0b317ca738
Struan Donald <struan@exo.org.uk>
2022-12-01 13:46:28 +0000

    reformat with black

--- import_petitions.py
+++ import_petitions.py
@@ -4,7 +4,7 @@ Import petitions data from petitions.parliament.uk
 """
 
 from time import sleep
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import parse_qs, urlparse
 
 import requests
 from sqlite_utils import Database
@@ -14,51 +14,45 @@ def main():
     keywords = []
 
     try:
-        with open('keywords.csv') as f:
-            keywords = [ l.strip() for l in f ]
+        with open("keywords.csv") as f:
+            keywords = [l.strip() for l in f]
     except FileNotFoundError:
         print("Error: keywords.csv file not found. Aborting.")
         return 1
-    
-    print("Loaded {} keywords from keywords.csv: {}".format(
-        len(keywords),
-        ', '.join(keywords)
-    ))
 
-    db = Database('data.sqlite')
+    print(
+        "Loaded {} keywords from keywords.csv: {}".format(
+            len(keywords), ", ".join(keywords)
+        )
+    )
+
+    db = Database("data.sqlite")
 
     for keyword in keywords:
         next_page = 1
         while next_page > 0:
-            r = requests.get('https://petition.parliament.uk/petitions.json', params={
-                'q': keyword,
-                'page': next_page
-            })
+            r = requests.get(
+                "https://petition.parliament.uk/petitions.json",
+                params={"q": keyword, "page": next_page},
+            )
 
             assert r.status_code == 200, "{} returned status code {}".format(
-                r.url,
-                r.status_code
+                r.url, r.status_code
             )
 
             print(r.url)
 
             j = r.json()
 
-            for p in j['data']:
+            for p in j["data"]:
                 petition = extract_petition_data(p)
 
-                db['petitions'].upsert(
-                    petition,
-                    pk='id'
-                ).m2m(
-                    'keywords',
-                    lookup={
-                        'keyword': keyword
-                    }
+                db["petitions"].upsert(petition, pk="id").m2m(
+                    "keywords", lookup={"keyword": keyword}
                 )
 
-            if j['links']['next']:
-                next_page = get_page_number_from_url(j['links']['next'])
+            if j["links"]["next"]:
+                next_page = get_page_number_from_url(j["links"]["next"])
             else:
                 next_page = 0
 
@@ -67,25 +61,27 @@ def main():
 
 def extract_petition_data(p):
     petition = {
-        'id': p['id'],
-        'url': p['links']['self'],
-        'state': p['attributes']['state'],
-        'action': p['attributes']['action'],
-        'background': p['attributes']['background'],
-        'additional_details': p['attributes']['additional_details'],
-        'signature_count': p['attributes']['signature_count'],
-        'date_created': p['attributes']['created_at'],
+        "id": p["id"],
+        "url": p["links"]["self"],
+        "state": p["attributes"]["state"],
+        "action": p["attributes"]["action"],
+        "background": p["attributes"]["background"],
+        "additional_details": p["attributes"]["additional_details"],
+        "signature_count": p["attributes"]["signature_count"],
+        "date_created": p["attributes"]["created_at"],
     }
 
     try:
-        petition['date_responded'] = p['attributes']['government_response']['responded_on']
+        petition["date_responded"] = p["attributes"]["government_response"][
+            "responded_on"
+        ]
     except TypeError:
-        petition['date_responded'] = None
-    
+        petition["date_responded"] = None
+
     try:
-        petition['date_debated'] = p['attributes']['debate']['debated_on']
+        petition["date_debated"] = p["attributes"]["debate"]["debated_on"]
     except TypeError:
-        petition['date_debated'] = None
+        petition["date_debated"] = None
 
     return petition
 
@@ -93,7 +89,7 @@ def extract_petition_data(p):
 def get_page_number_from_url(url):
     u = urlparse(url)
     q = parse_qs(u.query)
-    return int(q['page'][0])
+    return int(q["page"][0])
 
 
 if __name__ == "__main__":
