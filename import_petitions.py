e04ff34aca5961e82108b5b59b136c8d9470ad34
Struan Donald <struan@exo.org.uk>
2022-12-01 13:46:37 +0000

    fetch petition signature counts per constituency and summarize
    iterates over all the petitions in the DB, fetches their signature
    counts per constituency, saves those.
    
    Outputs a json file with the details of the top three petitions per
    constituency

--- import_petitions.py
+++ import_petitions.py
@@ -3,6 +3,8 @@
 Import petitions data from petitions.parliament.uk
 """
 
+import json
+from collections import defaultdict
 from time import sleep
 from urllib.parse import parse_qs, urlparse
 
@@ -57,6 +59,46 @@ def main():
                 next_page = 0
 
             sleep(1)
+
+    large_petitions = db.query("select * from petitions where signature_count >= 10000")
+
+    for petition in large_petitions:
+        print(petition["url"])
+        r = requests.get(petition["url"])
+        if r.status_code != 200:
+            print(f"failed to fetch {petition['url']}: {r.status_code}")
+            continue
+
+        j = r.json()
+        for cons in j["data"]["attributes"]["signatures_by_constituency"]:
+            sigs = {
+                "petition_id": petition["id"],
+                "constituency": cons["name"],
+                "gss": cons["ons_code"],
+                "signatures": cons["signature_count"],
+            }
+
+            db["constituency_signatures"].upsert(
+                sigs,
+                pk=("gss", "petition_id"),
+                foreign_keys=[("petition_id", "petitions", "id")],
+            )
+
+        sleep(1)
+
+    cons = db.query("select distinct(gss) from constituency_signatures")
+
+    top_petitions = defaultdict(list)
+    for con in cons:
+        petitions = db.query(
+            "select petitions.*, constituency_signatures.* from petitions, constituency_signatures where constituency_signatures.petition_id = petitions.id and gss = ? order by signatures desc limit 3",
+            [con["gss"]],
+        )
+        for petition in petitions:
+            top_petitions[con["gss"]].append(petition)
+
+    with open("top_three_per_cons.json", "w") as output:
+        json.dump(top_petitions, output)
 
 
 def extract_petition_data(p):
